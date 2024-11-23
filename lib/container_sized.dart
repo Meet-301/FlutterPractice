6354c129439d19d9e201d49a330bdeb2573bf4d1
Meet-301 <pujarameet301@gmail.com>
2024-11-23 11:49:41 +0530

    container and sized box

---
+++ lib/container_sized.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class container_sized extends StatelessWidget {
+  const container_sized({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(
+       title: const Text("Container and Sized Box"),
+     ),
+   );
+  }
+}
